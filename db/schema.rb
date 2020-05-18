29134248fd4e591875ec4649940e2fbb98e2b618
Louise Norris <louisenorris73@yahoo.co.uk>
2020-05-18 12:40:43 +0100

    added postcode to artworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_100512) do
+ActiveRecord::Schema.define(version: 2020_05_18_111453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_100512) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.string "description"
+    t.string "postcode"
   end
 
   create_table "tour_artworks", force: :cascade do |t|
